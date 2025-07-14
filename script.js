0ef0b3373804321658db02673c077d80409274ff
moha750 <you@example.com>
2025-07-14 15:14:57 +0300

    تحديث جديد

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(registration => console.log('ServiceWorker registered'))
-      .catch(err => console.log('ServiceWorker registration failed: ', err));
-  });
-}
-
 // بيانات المهارات الثابتة
 // عدد المشاريع المطلوب لكل حجم شاشة
 const projectsToShow = {
